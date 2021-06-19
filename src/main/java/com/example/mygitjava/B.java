0411e98eaec0c016ccdc275449f186b016df5d6a
angel107 <lkj1225@skuniv.ac.kr>
2021-06-19 18:34:45 +0900

    add a new line in A,B

--- src/main/java/com/example/mygitjava/B.java
+++ src/main/java/com/example/mygitjava/B.java
@@ -3,5 +3,6 @@ package com.example.mygitjava;
 public class B {
     public static void main(String[] args) {
         System.out.println("this is a B.java");
+        System.out.println("add a new line in A,B");
     }
 }
